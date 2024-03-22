c09c1d6ead0261d8c81e1586eff26ff9cbeb4666
Hye0nw0O <hw4151@naver.com>
2024-03-22 15:52:49 +0900

    ossp1 branch에서 수정

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 name = input("이름을 입력하세요: ")
 num = input("학번을 입력하세요: ")
+team = input("팀명을 입력하세요: ")
 
 print("<출력>")
-print("이름", name)
-print("학번", num)
+print("이름: ", name)
+print("학번: ", num)
+print("팀명: ", team)
